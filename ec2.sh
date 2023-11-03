f69cc81a7e35cb6f346dc98312bb02a4674e8e73
nuthakkinarendra <70637778+nuthakkinarendra@users.noreply.github.com>
2023-11-03 13:43:27 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-aws ec2 run-instances --image-id ami-0fc5d935ebf8bc3bc --count 1 --instance-type t2.micro --key-name jenkins --security-group-ids sg-0c30281be9b5d414f --region us-east-1
+aws ec2 run-instances --image-id ami-0fe8bec493a81c7da --count 1 --instance-type t2.micro --key-name practice --security-group-ids sg-00fa347f03b792c44 --region us-east-1
 
 echo "$count EC2 instances have been successfully created. Please check management console"
